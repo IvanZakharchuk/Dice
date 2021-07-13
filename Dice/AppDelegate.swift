f8b7accabd3189e429a26de73df95e6a670ec6f5
Іван Захарчук <john.zacharchuk@gmail.com>
2021-07-13 11:42:54 +0300

    first commit

---
+++ Dice/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Dice
+//
+//  Created by Іван Захарчук on 12.07.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    )
+        -> Bool
+    {
+        return true
+    }
+}
+
