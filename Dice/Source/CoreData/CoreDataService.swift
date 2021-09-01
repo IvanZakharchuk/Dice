7772017ff5b51adc5a7a72a4382407925dc7f1b0
Іван Захарчук <john.zacharchuk@gmail.com>
2021-09-01 11:15:53 +0300

    CoreDataService

--- Dice/Source/CoreData/CoreDataService.swift
+++ Dice/Source/CoreData/CoreDataService.swift
@@ -124,3 +124,48 @@ class CoreDataManager: CRUD {
         }
     }
 }
+
+class CoreDataService: CRUD {
+    
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreData")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    public func create(player: CoreDataPlayer) {
+        self.saveContext()
+    }
+    
+    public func read(player: CoreDataPlayer) {
+        
+    }
+    
+    public func update(player: CoreDataPlayer) {
+        
+    }
+    
+    public func delete(player: CoreDataPlayer) {
+        
+    }
+}
